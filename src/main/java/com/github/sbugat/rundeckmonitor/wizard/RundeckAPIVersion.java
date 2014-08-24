4ef747a02e1fdb3a52c5a58db4df470f3bf25f27
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-08-24 22:11:09 +0200

    Add rundeck API version validation

---
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/RundeckAPIVersion.java
@@ -0,0 +1,37 @@
+package com.github.sbugat.rundeckmonitor.wizard;
+
+import org.rundeck.api.RundeckClient.Version;
+
+public enum RundeckAPIVersion {
+
+	RUNDECK_APIVERSION_5( Version.V5, "1.4.4" ), //$NON-NLS-1$
+	RUNDECK_APIVERSION_6( Version.V6, "1.4.4" ), //$NON-NLS-1$
+	RUNDECK_APIVERSION_7( Version.V7, "1.4.4" ), //$NON-NLS-1$
+	RUNDECK_APIVERSION_8( Version.V8, "1.4.4" ), //$NON-NLS-1$
+	RUNDECK_APIVERSION_9( Version.V9, "1.4.4" ), //$NON-NLS-1$
+	RUNDECK_APIVERSION_10( Version.V10, "1.4.4" ), //$NON-NLS-1$
+	RUNDECK_APIVERSION_11( Version.V11, "1.4.4" ); //$NON-NLS-1$
+
+	/**Rundeck API Version*/
+	private final Version version;
+	/**First version using this API version*/
+	private final String sinceRundeckVersion;
+
+	private RundeckAPIVersion( final Version versionArg, final String sinceRundeckVersionArg ) {
+
+		version = versionArg;
+		sinceRundeckVersion = sinceRundeckVersionArg;
+	}
+
+	public Version getVersion() {
+		return version;
+	}
+
+	public String getSinceReturnVersion() {
+		return sinceRundeckVersion;
+	}
+
+	public String toString() {
+		return version + " - " + sinceRundeckVersion; //$NON-NLS-1$
+	}
+}
