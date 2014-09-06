ec7927b5fcd8075115d53dec280d826698d18aa9
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-09-06 15:15:20 +0200

    WIP, opening job redirection

--- src/main/java/com/github/sbugat/rundeckmonitor/wizard/JobTabRedirection.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/JobTabRedirection.java
@@ -2,13 +2,13 @@ package com.github.sbugat.rundeckmonitor.wizard;
 
 public enum JobTabRedirection {
 
-	SUMMARY( "show", "", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	REPORT( "show", "#state", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	LOG_OUTPUT( "show", "#output", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	DEFINITION( "show", "", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	RENDER_OUTPUT_TXT( "downloadOutput", "", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	RENDER_OUTPUT_HTML( "show", "", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	DOWNLOAD_OUTPUT( "downloadOutput", "", "summary" ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	SUMMARY( "show", "", "Exceution summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	//REPORT( "show", "#state", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	//LOG_OUTPUT( "show", "#output", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	//DEFINITION( "show", "", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	RENDER_OUTPUT_TXT( "downloadOutput", "?view=inline&formatted=false&stripansi=true", "View raw log" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	RENDER_OUTPUT_HTML( "renderOutput", "?ansicolor=on&loglevels=on", "View html formated log" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	DOWNLOAD_OUTPUT( "downloadOutput", "", "Download raw log" ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 	private final String accessUrlPrefix;
 
@@ -23,6 +23,14 @@ public enum JobTabRedirection {
 		description = descriptionArg;
 	}
 
+	public String getAccessUrlPrefix() {
+		return accessUrlPrefix;
+	}
+
+
+	public String getAccessUrlSuffix() {
+		return accessUrlSuffix;
+	}
 
 	public String toString() {
 
