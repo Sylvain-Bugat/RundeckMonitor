462ab030fc92dd7104db38be805f1280c3bd7452
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-26 00:20:18 +0100

    Checkstyle fixes

--- src/main/java/com/github/sbugat/rundeckmonitor/wizard/InterfaceType.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/InterfaceType.java
@@ -1,25 +1,36 @@
 package com.github.sbugat.rundeckmonitor.wizard;
 
 /**
- * Possible value of Java interface type
+ * Possible value of Java interface type.
  *
  * @author Sylvain Bugat
  *
  */
 public enum InterfaceType {
 
-	/**AWT interface for non-Windows system if Swing don't work or if AWT is prefered*/
+	/**AWT interface for non-Windows system if Swing don't work or if AWT is prefered.*/
 	AWT( "AWT" ), //$NON-NLS-1$
-	/**Swing interface for Windows system*/
+	/**Swing interface for Windows system.*/
 	SWING( "Swing" ); //$NON-NLS-1$
 
+	/**Displayed String of an interface type.*/
 	private final String interfaceType;
 
+	/**
+	 * Type of a GUI interface.
+	 *
+	 * @param interfaceTypeArg displayed String of an interface type
+	 */
 	private InterfaceType( final String interfaceTypeArg ) {
 
 		interfaceType = interfaceTypeArg;
 	}
 
+	/**
+	 * return the String to display for this interface type.
+	 *
+	 * @return interface type
+	 */
 	public String getInterfaceType() {
 		return interfaceType;
 	}
