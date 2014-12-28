621d55419b9fbb54c7c9c51e696f42c61d6d6ea4
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-28 23:59:28 +0100

    Code formatting

--- src/main/java/com/github/sbugat/rundeckmonitor/tools/RundeckClientTools.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/tools/RundeckClientTools.java
@@ -9,14 +9,14 @@ import com.github.sbugat.rundeckmonitor.configuration.RundeckMonitorConfiguratio
 
 /**
  * Tools class for building rundeck clients.
- *
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
 public final class RundeckClientTools {
 
-	/**SLF4J XLogger.*/
-	private static final XLogger LOG = XLoggerFactory.getXLogger( RundeckClientTools.class );
+	/** SLF4J XLogger. */
+	private static final XLogger LOG = XLoggerFactory.getXLogger(RundeckClientTools.class);
 
 	/**
 	 * Private constructor to prevent Instantiating.
@@ -27,49 +27,49 @@ public final class RundeckClientTools {
 
 	/**
 	 * Build a rundeck client with the argument configuration.
-	 *
+	 * 
 	 * @param rundeckMonitorConfiguration configuration to use
 	 * @param minimalRundeckAPIVersion use API version 1 if true
 	 * @return build rundeck client
 	 */
-	public static RundeckClient buildRundeckClient( final RundeckMonitorConfiguration rundeckMonitorConfiguration, final boolean minimalRundeckAPIVersion ) {
+	public static RundeckClient buildRundeckClient(final RundeckMonitorConfiguration rundeckMonitorConfiguration, final boolean minimalRundeckAPIVersion) {
 
-		LOG.entry( rundeckMonitorConfiguration, minimalRundeckAPIVersion );
+		LOG.entry(rundeckMonitorConfiguration, minimalRundeckAPIVersion);
 
 		final String rundeckUrl = rundeckMonitorConfiguration.getRundeckUrl();
 
-		//Client builder using API token if it is present, otherwise use login and password
+		// Client builder using API token if it is present, otherwise use login and password
 		final RundeckClientBuilder rundeckClientBuilder;
-		if( null != rundeckMonitorConfiguration.getRundeckAPIKey() && ! rundeckMonitorConfiguration.getRundeckAPIKey().isEmpty() ) {
-			LOG.info( "Generate a new RunDeck client to {} using API token authentication", rundeckUrl ); //$NON-NLS-1$
-			rundeckClientBuilder = RundeckClient.builder().url( rundeckUrl ).token( rundeckMonitorConfiguration.getRundeckAPIKey() );
+		if (null != rundeckMonitorConfiguration.getRundeckAPIKey() && !rundeckMonitorConfiguration.getRundeckAPIKey().isEmpty()) {
+			LOG.info("Generate a new RunDeck client to {} using API token authentication", rundeckUrl); //$NON-NLS-1$
+			rundeckClientBuilder = RundeckClient.builder().url(rundeckUrl).token(rundeckMonitorConfiguration.getRundeckAPIKey());
 		}
 		else {
-			LOG.info( "Generate a new RunDeck client to {} using login: {} and password authentication", rundeckUrl, rundeckMonitorConfiguration.getRundeckLogin() ); //$NON-NLS-1$
-			rundeckClientBuilder = RundeckClient.builder().url( rundeckUrl ).login( rundeckMonitorConfiguration.getRundeckLogin(), rundeckMonitorConfiguration.getRundeckPassword() );
+			LOG.info("Generate a new RunDeck client to {} using login: {} and password authentication", rundeckUrl, rundeckMonitorConfiguration.getRundeckLogin()); //$NON-NLS-1$
+			rundeckClientBuilder = RundeckClient.builder().url(rundeckUrl).login(rundeckMonitorConfiguration.getRundeckLogin(), rundeckMonitorConfiguration.getRundeckPassword());
 		}
 
 		final RundeckClient rundeckClient;
-		if( minimalRundeckAPIVersion ) {
-			LOG.info( "Generate a new RunDeck client using API v1 for compatibility issue" ); //$NON-NLS-1$
-			//Initialize the rundeck client with the minimal rundeck version (1)
-			rundeckClient = rundeckClientBuilder.version( 1 ).build();
+		if (minimalRundeckAPIVersion) {
+			LOG.info("Generate a new RunDeck client using API v1 for compatibility issue"); //$NON-NLS-1$
+			// Initialize the rundeck client with the minimal rundeck version (1)
+			rundeckClient = rundeckClientBuilder.version(1).build();
 		}
 		else {
-			LOG.info( "Generate a new RunDeck client using API v{}", rundeckMonitorConfiguration.getRundeckAPIversion() ); //$NON-NLS-1$
-			//Use the configured RunDeck API version
-			rundeckClient = rundeckClientBuilder.version( rundeckMonitorConfiguration.getRundeckAPIversion() ).build();
+			LOG.info("Generate a new RunDeck client using API v{}", rundeckMonitorConfiguration.getRundeckAPIversion()); //$NON-NLS-1$
+			// Use the configured RunDeck API version
+			rundeckClient = rundeckClientBuilder.version(rundeckMonitorConfiguration.getRundeckAPIversion()).build();
 		}
 
-		//Test connection and authentication credentials
-		LOG.debug( "Test the connection to RunDeck" ); //$NON-NLS-1$
+		// Test connection and authentication credentials
+		LOG.debug("Test the connection to RunDeck"); //$NON-NLS-1$
 		rundeckClient.ping();
-		LOG.debug( "Connection  to RunDeck OK, test authentication" ); //$NON-NLS-1$
+		LOG.debug("Connection  to RunDeck OK, test authentication"); //$NON-NLS-1$
 		rundeckClient.testAuth();
-		LOG.debug( "Connection and authentication OK" ); //$NON-NLS-1$
+		LOG.debug("Connection and authentication OK"); //$NON-NLS-1$
 
-		//Everything is ok, return the client
-		LOG.exit( rundeckClient );
+		// Everything is ok, return the client
+		LOG.exit(rundeckClient);
 		return rundeckClient;
 	}
 }
