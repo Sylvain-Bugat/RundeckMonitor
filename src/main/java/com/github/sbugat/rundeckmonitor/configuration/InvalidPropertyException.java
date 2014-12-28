a400cbe01b1a5fa55a2bd64fe1835192f4508601
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-28 23:42:53 +0100

    CheckStyle fixes and code formatting

--- src/main/java/com/github/sbugat/rundeckmonitor/configuration/InvalidPropertyException.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/configuration/InvalidPropertyException.java
@@ -1,30 +1,46 @@
 package com.github.sbugat.rundeckmonitor.configuration;
 
 /**
- * Exception to use when a property is invalid (value type mismatch or value mismatch)
- *
+ * Exception to use when a property is invalid (value type mismatch or value mismatch).
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
-public class InvalidPropertyException extends Exception{
+public class InvalidPropertyException extends Exception {
 
 	private static final long serialVersionUID = 4577273435503511931L;
 
-	/**Name of the invalid property */
+	/** Name of the invalid property. */
 	private final String property;
 
-	/**Value of the invalid property */
+	/** Value of the invalid property. */
 	private final String propertyValue;
 
-	public InvalidPropertyException( final String propertyArg, final String propertyValueArg ){
+	/**
+	 * Constructor copy property name and his value
+	 * 
+	 * @param propertyArg property name
+	 * @param propertyValueArg value of the property
+	 */
+	public InvalidPropertyException(final String propertyArg, final String propertyValueArg) {
 		property = propertyArg;
 		propertyValue = propertyValueArg;
 	}
 
+	/**
+	 * Get the property name.
+	 * 
+	 * @return property name
+	 */
 	public String getProperty() {
 		return property;
 	}
 
+	/**
+	 * Get the property value.
+	 * 
+	 * @return property value
+	 */
 	public String getPropertyValue() {
 		return propertyValue;
 	}
--- src/main/java/com/github/sbugat/rundeckmonitor/configuration/MissingPropertyException.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/configuration/MissingPropertyException.java
@@ -1,22 +1,32 @@
 package com.github.sbugat.rundeckmonitor.configuration;
 
 /**
- * Exception to use when a mandatory property is missing
- *
+ * Exception to use when a mandatory property is missing.
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
 public class MissingPropertyException extends Exception {
 
 	private static final long serialVersionUID = -4199859651106152630L;
 
-	/**Name of the missing property */
+	/** Name of the missing property. */
 	private final String property;
 
-	public MissingPropertyException( final String propertyArg ){
+	/**
+	 * Constructor copy the missing property name.
+	 * 
+	 * @param propertyArg missing property name
+	 */
+	public MissingPropertyException(final String propertyArg) {
 		property = propertyArg;
 	}
 
+	/**
+	 * Get the missing property name.
+	 * 
+	 * @return missing property name
+	 */
 	public String getProperty() {
 		return property;
 	}
