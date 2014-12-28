1bac125093d31cbdc1ff3c3c90216c3b8e223450
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-28 23:58:28 +0100

    Code formatting and add comments

--- src/main/java/com/github/sbugat/rundeckmonitor/tools/EnvironmentTools.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/tools/EnvironmentTools.java
@@ -7,16 +7,18 @@ import org.slf4j.ext.XLoggerFactory;
 
 /**
  * Tools class to detect Operating System type.
- *
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
 public final class EnvironmentTools {
 
-	/**SLF4J XLogger.*/
-	private static final XLogger LOG = XLoggerFactory.getXLogger( EnvironmentTools.class );
+	/** SLF4J XLogger. */
+	private static final XLogger LOG = XLoggerFactory.getXLogger(EnvironmentTools.class);
 
+	/** Windows OS name property. */
 	private static final String OS_NAME_PROPERTY = "os.name"; //$NON-NLS-1$
+	/** Windows name in OS name property. */
 	private static final String WINDOWS_OS_NAME = "windows"; //$NON-NLS-1$
 
 	/**
@@ -28,21 +30,21 @@ public final class EnvironmentTools {
 
 	/**
 	 * Check if the operating system is a windows based on a property, otherwise it's a Linux/Mac-OS.
-	 *
+	 * 
 	 * @return true if the OS is a windows*
 	 */
 	public static boolean isWindows() {
 
 		LOG.entry();
-		final String operatingSystem = System.getProperty( OS_NAME_PROPERTY );
+		final String operatingSystem = System.getProperty(OS_NAME_PROPERTY);
 
-		if( null == operatingSystem ) {
-			LOG.exit( false );
+		if (null == operatingSystem) {
+			LOG.exit(false);
 			return false;
 		}
 
-		final boolean isWindows = operatingSystem.toLowerCase( Locale.getDefault() ).startsWith( WINDOWS_OS_NAME );
-		LOG.exit( isWindows );
+		final boolean isWindows = operatingSystem.toLowerCase(Locale.getDefault()).startsWith(WINDOWS_OS_NAME);
+		LOG.exit(isWindows);
 		return isWindows;
 	}
 }
