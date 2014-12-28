c6bff65503b41644c7f2792354afedd23c215850
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-28 23:55:14 +0100

    CheckStyle fixes and code formatting

--- src/main/java/com/github/sbugat/rundeckmonitor/configuration/UnknownProjectException.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/configuration/UnknownProjectException.java
@@ -1,22 +1,32 @@
 package com.github.sbugat.rundeckmonitor.configuration;
 
 /**
- * Exception to use when an unknow project is configured
- *
+ * Exception to use when an unknow project is configured.
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
-public class UnknownProjectException extends Exception{
+public class UnknownProjectException extends Exception {
 
 	private static final long serialVersionUID = -2516928050086861224L;
 
-	/**Name of the unknown project*/
+	/** Name of the unknown project. */
 	private final String projectName;
 
-	public UnknownProjectException( final String projectNameArg ){
+	/**
+	 * Exception constructor of an unknown project error.
+	 * 
+	 * @param projectNameArg unknown project name
+	 */
+	public UnknownProjectException(final String projectNameArg) {
 		projectName = projectNameArg;
 	}
 
+	/**
+	 * Get the unknown project name.
+	 * 
+	 * @return unknown project name
+	 */
 	public String getProjectName() {
 		return projectName;
 	}
