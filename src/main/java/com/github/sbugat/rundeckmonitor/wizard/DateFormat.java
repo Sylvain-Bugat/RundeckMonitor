9c38c7840ade5f8d6540cfb351e045f3b7efe533
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-28 23:49:37 +0100

    CheckStyle fixes and code formatting.

--- src/main/java/com/github/sbugat/rundeckmonitor/wizard/DateFormat.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/DateFormat.java
@@ -4,35 +4,54 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * Possible standard date format for the wizard
- *
+ * Possible standard date format for the wizard.
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
 public enum DateFormat {
 
-	DATE_FORMAT_STANDARD( "dd/MM/yyyy HH:mm:ss" ), //$NON-NLS-1$
-	DATE_FORMAT_UNITED_STATES( "MM/dd/yyyy HH:mm:ss" ), //$NON-NLS-1$
-	DATE_FORMAT_EAST_ASIA( "yyyy/MM/dd HH:mm:ss" ); //$NON-NLS-1$
+	/** European date format. */
+	DATE_FORMAT_STANDARD("dd/MM/yyyy HH:mm:ss"), //$NON-NLS-1$
+	/** North America date format. */
+	DATE_FORMAT_UNITED_STATES("MM/dd/yyyy HH:mm:ss"), //$NON-NLS-1$
+	/** East Asia date format. */
+	DATE_FORMAT_EAST_ASIA("yyyy/MM/dd HH:mm:ss"); //$NON-NLS-1$
 
-	/**Date format to use*/
+	/** Date format to use. */
 	private final String dateFormat;
-	/**Example of the current date to display*/
+	/** Example of the current date to display. */
 	private final String example;
 
-	private DateFormat( final String dateFormatArg ) {
+	/**
+	 * Enumeration constructor with an example of the format with the system date.
+	 * 
+	 * @param dateFormatArg date format
+	 */
+	private DateFormat(final String dateFormatArg) {
 
 		dateFormat = dateFormatArg;
 
-		final SimpleDateFormat formatter = new SimpleDateFormat( dateFormat );
-		example = formatter.format( new Date() );
+		final SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+		example = formatter.format(new Date());
 	}
 
+	/**
+	 * Get the date format.
+	 * 
+	 * @return the date format.
+	 */
 	public String getDateFormat() {
 		return dateFormat;
 	}
 
+	/**
+	 * Return a String to display this date format.
+	 * 
+	 * @return String to display
+	 */
+	@Override
 	public String toString() {
-		return dateFormat + " - "  + example ; //$NON-NLS-1$
+		return dateFormat + " - " + example; //$NON-NLS-1$
 	}
 }
