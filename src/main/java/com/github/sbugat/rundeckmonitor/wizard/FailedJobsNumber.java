bb786782adbe66e1b2839e22bf0b5b12fbf41392
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-29 23:43:26 +0100

    Code formatting and add comments

--- src/main/java/com/github/sbugat/rundeckmonitor/wizard/FailedJobsNumber.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/FailedJobsNumber.java
@@ -1,43 +1,75 @@
 package com.github.sbugat.rundeckmonitor.wizard;
 
 /**
- * Possible value of possible failed and late jobs numbers for the wizard
- *
+ * Possible value of possible failed and late jobs numbers for the wizard.
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
 public enum FailedJobsNumber {
 
-	FAILED_JOBS_5( 5 ),
-	FAILED_JOBS_6( 6 ),
-	FAILED_JOBS_7( 7 ),
-	FAILED_JOBS_8( 8 ),
-	FAILED_JOBS_9( 9 ),
-	FAILED_JOBS_10( 10 ),
-	FAILED_JOBS_11( 11 ),
-	FAILED_JOBS_12( 12 ),
-	FAILED_JOBS_13( 13 ),
-	FAILED_JOBS_14( 14 ),
-	FAILED_JOBS_15( 15 ),
-	FAILED_JOBS_16( 16 ),
-	FAILED_JOBS_17( 17 ),
-	FAILED_JOBS_18( 18 ),
-	FAILED_JOBS_19( 19 ),
-	FAILED_JOBS_20( 20 );
+	/** 5 failed jobs. */
+	FAILED_JOBS_5(5),
+	/** 6 failed jobs. */
+	FAILED_JOBS_6(6),
+	/** 7 failed jobs. */
+	FAILED_JOBS_7(7),
+	/** 8 failed jobs. */
+	FAILED_JOBS_8(8),
+	/** 9 failed jobs. */
+	FAILED_JOBS_9(9),
+	/** 10 failed jobs. */
+	FAILED_JOBS_10(10),
+	/** 11 failed jobs. */
+	FAILED_JOBS_11(11),
+	/** 12 failed jobs. */
+	FAILED_JOBS_12(12),
+	/** 13 failed jobs. */
+	FAILED_JOBS_13(13),
+	/** 14 failed jobs. */
+	FAILED_JOBS_14(14),
+	/** 15 failed jobs. */
+	FAILED_JOBS_15(15),
+	/** 16 failed jobs. */
+	FAILED_JOBS_16(16),
+	/** 17 failed jobs. */
+	FAILED_JOBS_17(17),
+	/** 18 failed jobs. */
+	FAILED_JOBS_18(18),
+	/** 19 failed jobs. */
+	FAILED_JOBS_19(19),
+	/** 20 failed jobs. */
+	FAILED_JOBS_20(20);
 
-	/**Number of failed/late jobs to display*/
+	/** Number of failed/late jobs to display. */
 	private final int failedJobsNumber;
 
-	private FailedJobsNumber( final int failedJobsNumberArg ) {
+	/**
+	 * Copy the failed job number argument.
+	 * 
+	 * @param failedJobsNumberArg failed job number to set
+	 */
+	private FailedJobsNumber(final int failedJobsNumberArg) {
 
-		failedJobsNumber= failedJobsNumberArg;
+		failedJobsNumber = failedJobsNumberArg;
 	}
 
+	/**
+	 * Get the failed job number.
+	 * 
+	 * @return failed job number
+	 */
 	public Integer getFailedJobsNumber() {
 		return failedJobsNumber;
 	}
 
+	/**
+	 * Convert the faild job number to a String.
+	 * 
+	 * @return displayed String
+	 */
+	@Override
 	public String toString() {
-		return String.valueOf( failedJobsNumber );
+		return String.valueOf(failedJobsNumber);
 	}
 }
--- src/main/java/com/github/sbugat/rundeckmonitor/wizard/RefreshDelay.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/RefreshDelay.java
@@ -1,35 +1,58 @@
 package com.github.sbugat.rundeckmonitor.wizard;
 
 /**
- * Possible value of the refresh delay for the wizard
- *
+ * Possible value of the refresh delay for the wizard.
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
 public enum RefreshDelay {
 
-	REFRESH_DELAY_10S( 10, "10 seconds" ), //$NON-NLS-1$
-	REFRESH_DELAY_30S( 30, "30 seconds" ), //$NON-NLS-1$
-	REFRESH_DELAY_1M( 60, "1 minute" ), //$NON-NLS-1$
-	REFRESH_DELAY_2M( 120, "2 minutes" ), //$NON-NLS-1$
-	REFRESH_DELAY_5M( 300, "5 minutes" ), //$NON-NLS-1$
-	REFRESH_DELAY_10M( 600, "10 minutes" ); //$NON-NLS-1$
+	/** 10 seconds refresh delay. */
+	REFRESH_DELAY_10S(10, "10 seconds"), //$NON-NLS-1$
+	/** 30 seconds refresh delay. */
+	REFRESH_DELAY_30S(30, "30 seconds"), //$NON-NLS-1$
+	/** 1 minute refresh delay. */
+	REFRESH_DELAY_1M(60, "1 minute"), //$NON-NLS-1$
+	/** 2 minutes refresh delay. */
+	REFRESH_DELAY_2M(120, "2 minutes"), //$NON-NLS-1$
+	/** 5 minutes refresh delay. */
+	REFRESH_DELAY_5M(300, "5 minutes"), //$NON-NLS-1$
+	/** 10 minutes refresh delay. */
+	REFRESH_DELAY_10M(600, "10 minutes"); //$NON-NLS-1$
 
-	/**Refresh delay*/
+	/** Refresh delay. */
 	private final Integer delay;
-	/**Translation in seconds/minutes of the delay*/
+	/** Translation in seconds/minutes of the delay. */
 	private final String description;
 
-	private RefreshDelay( final int delayArg, final String descriptionArg ) {
+	/**
+	 * RefreshDelay constructor copye arguments into constant values.
+	 * 
+	 * @param delayArg delay to set
+	 * @param descriptionArg descriptino to set
+	 */
+	private RefreshDelay(final int delayArg, final String descriptionArg) {
 
-		delay= Integer.valueOf( delayArg );
+		delay = Integer.valueOf(delayArg);
 		description = descriptionArg;
 	}
 
+	/**
+	 * Get the refresh delay.
+	 * 
+	 * @return refresh delay
+	 */
 	public Integer getDelay() {
 		return delay;
 	}
 
+	/**
+	 * Return the description to be displayed.
+	 * 
+	 * @return displayed String
+	 */
+	@Override
 	public String toString() {
 		return description;
 	}
