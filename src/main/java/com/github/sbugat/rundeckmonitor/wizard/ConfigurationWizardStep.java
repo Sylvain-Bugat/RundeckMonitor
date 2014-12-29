d75357606d565339eb8c1458b6fa4a8372df7e82
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-29 23:38:52 +0100

    Add comments

--- src/main/java/com/github/sbugat/rundeckmonitor/wizard/ConfigurationWizardStep.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/ConfigurationWizardStep.java
@@ -1,8 +1,17 @@
 package com.github.sbugat.rundeckmonitor.wizard;
 
+/**
+ * Steps of the configuration wizard.
+ * 
+ * @author Sylvain Bugat
+ * 
+ */
 public enum ConfigurationWizardStep {
 
+	/** RunDeck wizard step. */
 	RUNDECK_STEP,
+	/** RunDeck project wizard step. */
 	PROJECT_STEP,
+	/** RunDeck monitor wizard step. */
 	MONITOR_STEP;
 }
