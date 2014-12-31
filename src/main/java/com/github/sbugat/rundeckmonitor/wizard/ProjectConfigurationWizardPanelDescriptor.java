2b933a21533e6e0be9ecaf12dc2b96013a25dca0
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-12-31 18:38:26 +0100

    Code formatting and add comments

--- src/main/java/com/github/sbugat/rundeckmonitor/wizard/ProjectConfigurationWizardPanelDescriptor.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/ProjectConfigurationWizardPanelDescriptor.java
@@ -16,38 +16,53 @@ import org.rundeck.api.domain.RundeckProject;
 import com.github.sbugat.rundeckmonitor.configuration.RundeckMonitorConfiguration;
 import com.github.sbugat.rundeckmonitor.tools.RundeckClientTools;
 
-public class ProjectConfigurationWizardPanelDescriptor extends WizardPanelDescriptor {
+/**
+ * Project wizard panel.
+ * 
+ * @author Sylvain Bugat
+ * 
+ */
+public final class ProjectConfigurationWizardPanelDescriptor extends WizardPanelDescriptor {
 
-	final Container container = new Container();
-	final GridBagLayout layout = new GridBagLayout();
+	/** Main container. */
+	private final Container container = new Container();
 
-	final JComboBox<String> rundeckProjectNameTextField = new JComboBox<>();
+	/** Combo box for selecting the RunDeck project. */
+	private final JComboBox<String> rundeckProjectNameTextField = new JComboBox<>();
 
-	final JComboBox<RundeckAPIVersion> rundeckRundeckAPIVersionTextField = new JComboBox<>();
+	/** Combo box for selecting the RunDeck API version. */
+	private final JComboBox<RundeckAPIVersion> rundeckRundeckAPIVersionTextField = new JComboBox<>();
 
-	public ProjectConfigurationWizardPanelDescriptor( final ConfigurationWizardStep panelIdentifierArg, final ConfigurationWizardStep backArg, final ConfigurationWizardStep nextArg, final RundeckMonitorConfiguration rundeckMonitorConfigurationArg ) {
-		super( panelIdentifierArg, backArg, nextArg, rundeckMonitorConfigurationArg );
+	/**
+	 * Copy arguments and initialize the RunDeck project configuration wizard panel.
+	 * 
+	 * @param backArg previous panel
+	 * @param nextArg next panel
+	 * @param rundeckMonitorConfigurationArg RunDeck monitor common configuration
+	 */
+	public ProjectConfigurationWizardPanelDescriptor(final ConfigurationWizardStep backArg, final ConfigurationWizardStep nextArg, final RundeckMonitorConfiguration rundeckMonitorConfigurationArg) {
+		super(ConfigurationWizardStep.PROJECT_STEP, backArg, nextArg, rundeckMonitorConfigurationArg);
 
-		container.setLayout( layout );
-		final JLabel rundeckProjectlabel = new JLabel( "Rundeck project:" ); //$NON-NLS-1$
-		final JLabel rundeckApiVersionlabel = new JLabel( "Rundeck API version:" ); //$NON-NLS-1$
+		container.setLayout(new GridBagLayout());
+		final JLabel rundeckProjectlabel = new JLabel("Rundeck project:"); //$NON-NLS-1$
+		final JLabel rundeckApiVersionlabel = new JLabel("Rundeck API version:"); //$NON-NLS-1$
 
 		final GridBagConstraints gridBagConstraits = new GridBagConstraints();
-		gridBagConstraits.insets = new Insets( 2,2,2,2 );
+		gridBagConstraits.insets = new Insets(2, 2, 2, 2);
 		gridBagConstraits.fill = GridBagConstraints.HORIZONTAL;
 		gridBagConstraits.gridwidth = 1;
 
-		gridBagConstraits.gridx=0;
-		gridBagConstraits.gridy=0;
-		container.add( rundeckProjectlabel, gridBagConstraits );
-		gridBagConstraits.gridx=1;
-		container.add( rundeckProjectNameTextField, gridBagConstraits );
+		gridBagConstraits.gridx = 0;
+		gridBagConstraits.gridy = 0;
+		container.add(rundeckProjectlabel, gridBagConstraits);
+		gridBagConstraits.gridx = 1;
+		container.add(rundeckProjectNameTextField, gridBagConstraits);
 
-		gridBagConstraits.gridx=0;
-		gridBagConstraits.gridy=1;
-		container.add( rundeckApiVersionlabel, gridBagConstraits );
-		gridBagConstraits.gridx=1;
-		container.add( rundeckRundeckAPIVersionTextField, gridBagConstraits );
+		gridBagConstraits.gridx = 0;
+		gridBagConstraits.gridy = 1;
+		container.add(rundeckApiVersionlabel, gridBagConstraits);
+		gridBagConstraits.gridx = 1;
+		container.add(rundeckRundeckAPIVersionTextField, gridBagConstraits);
 	}
 
 	@Override
@@ -56,78 +71,80 @@ public class ProjectConfigurationWizardPanelDescriptor extends WizardPanelDescri
 		return container;
 	}
 
+	@Override
 	public void aboutToDisplayPanel() {
 
-		//Initialize the rundeck client with the minimal rundeck version (1)
-		final RundeckClient rundeckClient = RundeckClientTools.buildRundeckClient( rundeckMonitorConfiguration, true );
+		// Initialize the rundeck client with the minimal rundeck version (1)
+		final RundeckClient rundeckClient = RundeckClientTools.buildRundeckClient(rundeckMonitorConfiguration, true);
 
 		rundeckProjectNameTextField.removeAllItems();
-		//Check if the configured project exists
+		// Check if the configured project exists
 		boolean existingOldConfiguredProject = false;
-		for( final RundeckProject rundeckProject: rundeckClient.getProjects() ) {
+		for (final RundeckProject rundeckProject : rundeckClient.getProjects()) {
 
 			final String currentProjectName = rundeckProject.getName();
-			rundeckProjectNameTextField.addItem( currentProjectName );
+			rundeckProjectNameTextField.addItem(currentProjectName);
 
-			if( ! currentProjectName.isEmpty() && currentProjectName.equals( rundeckMonitorConfiguration.getRundeckProject() ) ) {
+			if (!currentProjectName.isEmpty() && currentProjectName.equals(rundeckMonitorConfiguration.getRundeckProject())) {
 
 				existingOldConfiguredProject = true;
 			}
 		}
 
-		if( existingOldConfiguredProject ) {
-			rundeckProjectNameTextField.setSelectedItem( rundeckMonitorConfiguration.getRundeckProject() );
+		if (existingOldConfiguredProject) {
+			rundeckProjectNameTextField.setSelectedItem(rundeckMonitorConfiguration.getRundeckProject());
 		}
 
 		final String rundeckVersion = rundeckClient.getSystemInfo().getVersion();
 
 		rundeckRundeckAPIVersionTextField.removeAllItems();
 		RundeckAPIVersion oldApiVersion = null;
-		for( final RundeckAPIVersion version : RundeckAPIVersion.values() ) {
+		for (final RundeckAPIVersion version : RundeckAPIVersion.values()) {
 
-			if( rundeckVersion.compareTo( version.getSinceReturnVersion() ) >= 0  ) {
-				rundeckRundeckAPIVersionTextField.addItem( version );
+			if (rundeckVersion.compareTo(version.getSinceReturnVersion()) >= 0) {
+				rundeckRundeckAPIVersionTextField.addItem(version);
 
-				if( rundeckMonitorConfiguration.getRundeckAPIversion() > 0 ) {
-					if( version.getVersion().getVersionNumber() == rundeckMonitorConfiguration.getRundeckAPIversion() ) {
+				if (rundeckMonitorConfiguration.getRundeckAPIversion() > 0) {
+					if (version.getVersion().getVersionNumber() == rundeckMonitorConfiguration.getRundeckAPIversion()) {
 
 						oldApiVersion = version;
 					}
 				}
-				else if( version.getVersion().getVersionNumber() <= 10 ){
+				else if (version.getVersion().getVersionNumber() <= 12) {
 
 					oldApiVersion = version;
 				}
 			}
 		}
 
-		if( null != oldApiVersion ) {
-			rundeckRundeckAPIVersionTextField.setSelectedItem( oldApiVersion );
+		if (null != oldApiVersion) {
+			rundeckRundeckAPIVersionTextField.setSelectedItem(oldApiVersion);
 		}
 	}
 
+	@Override
 	public boolean validate() {
 
-		//Initialize the rundeck client
-		final RundeckClient rundeckClient = RundeckClientTools.buildRundeckClient( rundeckMonitorConfiguration, true );
+		// Initialize the rundeck client
+		final RundeckClient rundeckClient = RundeckClientTools.buildRundeckClient(rundeckMonitorConfiguration, true);
 
-		//Check if the configured project exists
+		// Check if the configured project exists
 		boolean existingProject = false;
-		for( final RundeckProject rundeckProject: rundeckClient.getProjects() ) {
+		for (final RundeckProject rundeckProject : rundeckClient.getProjects()) {
 
-			if( rundeckProjectNameTextField.getSelectedItem().equals( rundeckProject.getName() ) ) {
+			if (rundeckProjectNameTextField.getSelectedItem().equals(rundeckProject.getName())) {
 				existingProject = true;
 				break;
 			}
 		}
 
-		if( ! existingProject ) {
-			JOptionPane.showMessageDialog( null, "Unknown rundeck project," + System.lineSeparator() + "check and change this poject name:" + System.lineSeparator() + '"' + rundeckProjectNameTextField.getSelectedItem() + "\".", "RundeckMonitor wizard error", JOptionPane.ERROR_MESSAGE ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		if (!existingProject) {
+			JOptionPane.showMessageDialog(null, "Unknown rundeck project," + System.lineSeparator() + "check and change this poject name:" + System.lineSeparator() + '"' + rundeckProjectNameTextField.getSelectedItem() + "\".", "RundeckMonitor wizard error", JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 			return false;
 		}
 
-		rundeckMonitorConfiguration.setRundeckProject( rundeckProjectNameTextField.getItemAt( rundeckProjectNameTextField.getSelectedIndex() ) );
-		rundeckMonitorConfiguration.setRundeckAPIversion( rundeckRundeckAPIVersionTextField.getItemAt( rundeckRundeckAPIVersionTextField.getSelectedIndex() ).getVersion().getVersionNumber() );
+		rundeckMonitorConfiguration.setRundeckProject(rundeckProjectNameTextField.getItemAt(rundeckProjectNameTextField.getSelectedIndex()));
+		rundeckMonitorConfiguration.setRundeckAPIversion(rundeckRundeckAPIVersionTextField.getItemAt(rundeckRundeckAPIVersionTextField.getSelectedIndex()).getVersion().getVersionNumber());
 
 		return true;
 	}
