f02cb2e73e4c968a6018833231f4aaf31b3d76f1
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-01-01 23:59:19 +0100

    CheckStyle fixes and add comments

--- src/main/java/com/github/sbugat/rundeckmonitor/RundeckMonitorState.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/RundeckMonitorState.java
@@ -1,40 +1,73 @@
 package com.github.sbugat.rundeckmonitor;
 
 /**
- * Class containing the current state of the monitor
- *
+ * Class containing the current state of the monitor.
+ * 
  * @author Sylvain Bugat
- *
+ * 
  */
 public final class RundeckMonitorState {
 
+	/** Failed jobs flag. */
 	private boolean failedJobs;
 
+	/** Late/long jobs flag. */
 	private boolean lateJobs;
 
+	/** Disconnected flag. */
 	private boolean disconnected;
 
+	/**
+	 * Get the failed flag.
+	 * 
+	 * @return failed flag
+	 */
 	public boolean isFailedJobs() {
 		return failedJobs;
 	}
 
-	public void setFailedJobs( final boolean failedJobsArg ) {
+	/**
+	 * Set the failed flag.
+	 * 
+	 * @param failedJobsArg failed flag
+	 */
+	public void setFailedJobs(final boolean failedJobsArg) {
 		failedJobs = failedJobsArg;
 	}
 
+	/**
+	 * Get the long/late flag.
+	 * 
+	 * @return long/late flag
+	 */
 	public boolean isLateJobs() {
 		return lateJobs;
 	}
 
-	public void setLateJobs( final boolean lateJobsArg ) {
+	/**
+	 * Set the long/late flag.
+	 * 
+	 * @param lateJobsArg long/late flag
+	 */
+	public void setLateJobs(final boolean lateJobsArg) {
 		lateJobs = lateJobsArg;
 	}
 
+	/**
+	 * Get the disconnected flag.
+	 * 
+	 * @return true if disconnected
+	 */
 	public boolean isDisconnected() {
 		return disconnected;
 	}
 
-	public void setDisconnected( final boolean disconnectedArg ) {
+	/**
+	 * Set the disconnected state.
+	 * 
+	 * @param disconnectedArg disconnected state
+	 */
+	public void setDisconnected(final boolean disconnectedArg) {
 		disconnected = disconnectedArg;
 	}
 }
