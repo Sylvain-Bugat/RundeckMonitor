dbcae8ab0ff407773db15f87328cb4b37c805d45
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-01-01 23:59:27 +0100

    Code formatting

--- src/main/java/com/github/sbugat/rundeckmonitor/RundeckMonitorAboutDialog.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/RundeckMonitorAboutDialog.java
@@ -10,33 +10,33 @@ import javax.swing.UnsupportedLookAndFeelException;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 
-public class RundeckMonitorAboutDialog extends JFrame{
+public class RundeckMonitorAboutDialog extends JFrame {
 
-	private static final XLogger LOG = XLoggerFactory.getXLogger( RundeckMonitorAboutDialog.class );
+	private static final XLogger LOG = XLoggerFactory.getXLogger(RundeckMonitorAboutDialog.class);
 
 	private static final long serialVersionUID = 8614361410937565222L;
 
 	public RundeckMonitorAboutDialog() {
 
-		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		//Try to use the system Look&Feel
+		// Try to use the system Look&Feel
 		try {
-			UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
-		catch( final ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e ) {
+		catch (final ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
 
-			//If System Look&Feel is not supported, stay with the default one
-			LOG.warn( "Unsupported System Look&Feel", e ); //$NON-NLS-1$
+			// If System Look&Feel is not supported, stay with the default one
+			LOG.warn("Unsupported System Look&Feel", e); //$NON-NLS-1$
 		}
 
-		add( new JButton( "OK"), BorderLayout.SOUTH ); //$NON-NLS-1$
+		add(new JButton("OK"), BorderLayout.SOUTH); //$NON-NLS-1$
 
 		pack();
-		setVisible( true );
+		setVisible(true);
 	}
 
-	public static void main( final String[] args ) {
+	public static void main(final String[] args) {
 
 		new RundeckMonitorAboutDialog();
 	}
