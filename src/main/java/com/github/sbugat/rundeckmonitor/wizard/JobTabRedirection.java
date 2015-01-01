3eb2916d4b0484e42dd3c1a05c6fcb8a3208c796
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-01-01 23:48:49 +0100

    CheckStyle fixes and add comments

--- src/main/java/com/github/sbugat/rundeckmonitor/wizard/JobTabRedirection.java
+++ src/main/java/com/github/sbugat/rundeckmonitor/wizard/JobTabRedirection.java
@@ -1,25 +1,46 @@
 package com.github.sbugat.rundeckmonitor.wizard;
 
+/**
+ * Enumeration of possible execution default redirection.
+ * 
+ * @author Sylvain Bugat
+ * 
+ */
 public enum JobTabRedirection {
 
-	SUMMARY( "show", "", "Execution summary", "1.0" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	/** Summary of an execution. */
+	SUMMARY("show", "", "Execution summary", "1.0"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	//REPORT( "show", "#state", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	//LOG_OUTPUT( "show", "#output", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	//DEFINITION( "show", "", "summary" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	RENDER_OUTPUT_TXT( "downloadOutput", "?view=inline&formatted=false&stripansi=true", "View raw log", "2.0" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-	RENDER_OUTPUT_HTML( "renderOutput", "?ansicolor=on&loglevels=on", "View html formated log", "2.0" ), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-	DOWNLOAD_OUTPUT( "downloadOutput", "", "Download raw log", "1.0" ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	/** Display all logs of an execution as textual format. */
+	RENDER_OUTPUT_TXT("downloadOutput", "?view=inline&formatted=false&stripansi=true", "View raw log", "2.0"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	/** Display all logs of an execution as html format. */
+	RENDER_OUTPUT_HTML("renderOutput", "?ansicolor=on&loglevels=on", "View html formated log", "2.0"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	/** Download the log file of an execution. */
+	DOWNLOAD_OUTPUT("downloadOutput", "", "Download raw log", "1.0"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
+	/** First poart of the opened URL. */
 	private final String accessUrlPrefix;
 
+	/** Last part of the openened URL. */
 	private final String accessUrlSuffix;
 
+	/** Description for configuration. */
 	private final String description;
 
-	/**First version using this redirection*/
+	/** First version using this redirection. */
 	private final String sinceRundeckVersion;
 
-	private JobTabRedirection( final String accessUrlPrefixArg, final String accessUrlSuffixArg, final String descriptionArg, final String sinceRundeckVersionArg ) {
+	/**
+	 * Copy all arguments.
+	 * 
+	 * @param accessUrlPrefixArg URL prefix
+	 * @param accessUrlSuffixArg URL suffix
+	 * @param descriptionArg description
+	 * @param sinceRundeckVersionArg first version compatible with this redirection
+	 */
+	private JobTabRedirection(final String accessUrlPrefixArg, final String accessUrlSuffixArg, final String descriptionArg, final String sinceRundeckVersionArg) {
 
 		accessUrlPrefix = accessUrlPrefixArg;
 		accessUrlSuffix = accessUrlSuffixArg;
@@ -27,18 +48,39 @@ public enum JobTabRedirection {
 		sinceRundeckVersion = sinceRundeckVersionArg;
 	}
 
+	/**
+	 * get the URL prefix.
+	 * 
+	 * @return URL prefix
+	 */
 	public String getAccessUrlPrefix() {
 		return accessUrlPrefix;
 	}
 
+	/**
+	 * Get the URL suffix.
+	 * 
+	 * @return URL suffix
+	 */
 	public String getAccessUrlSuffix() {
 		return accessUrlSuffix;
 	}
 
+	/**
+	 * Get the first version
+	 * 
+	 * @return first version
+	 */
 	public String getSinceRundeckVersion() {
 		return sinceRundeckVersion;
 	}
 
+	/**
+	 * Get a redirection description.
+	 * 
+	 * @return redirection description
+	 */
+	@Override
 	public String toString() {
 		return description;
 	}
